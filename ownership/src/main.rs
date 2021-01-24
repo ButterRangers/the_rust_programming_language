7a554c8a3ed488394c4164d5a9c6a07e43f5265c
Per Eklund <perek586@gmail.com>
2021-01-24 11:39:27 +0100

    ownership in functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello");
+
+    let s = takes_ownership(s);
+
+    println!("we gotta take the power back {}", s); 
+
+    let x = 5;
+
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) -> String {
+    println!("{}", some_string);    
+    some_string
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
