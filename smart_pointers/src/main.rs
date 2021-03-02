280587a0819b97c16d4ccff0a76cb201a785010a
Per Eklund <perek586@gmail.com>
2021-03-02 17:27:20 +0100

    refernce count Rc<T>

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,21 +1,19 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer{
-    fn drop(&mut self){
-        println!("Dropping CustomSmartPointer with data '{}'", self.data);
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main(){
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a));
     }
-}
-
-fn main() {
-    let c = CustomSmartPointer {
-        data : String::from("my stuff"),
-    };
-    let _d = CustomSmartPointer {
-        data : String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created!");
-    drop(c);
-    println!("CustomSmartPointer dropped before the end of main ;)");
+    println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
 }
