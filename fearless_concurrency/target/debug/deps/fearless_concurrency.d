6ec74f01f4bc75d83d80ab511524925662def87d
Per Eklund <perek586@gmail.com>
2021-03-02 19:56:17 +0100

    concurrency stuff!

---
+++ fearless_concurrency/target/debug/deps/fearless_concurrency.d
@@ -0,0 +1,5 @@
+D:\Prog\Rust\the_rust_programming_language\fearless_concurrency\target\debug\deps\fearless_concurrency.exe: src\main.rs
+
+D:\Prog\Rust\the_rust_programming_language\fearless_concurrency\target\debug\deps\fearless_concurrency.d: src\main.rs
+
+src\main.rs:
---
+++ fearless_concurrency/target/debug/fearless_concurrency.d
@@ -0,0 +1,1 @@
+D:\Prog\Rust\the_rust_programming_language\fearless_concurrency\target\debug\fearless_concurrency.exe: D:\Prog\Rust\the_rust_programming_language\fearless_concurrency\src\main.rs
