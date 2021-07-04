2294cf71190b5ee2690114c922b0f2e812ee5bdb
Per Eklund <perek586@gmail.com>
2021-07-04 10:38:11 +0200

    didnt add the directory omg

---
+++ webserver_hello/target/debug/deps/webserver_hello.d
@@ -0,0 +1,5 @@
+D:\Prog\Rust\the_rust_programming_language\webserver_hello\target\debug\deps\webserver_hello.exe: src\main.rs
+
+D:\Prog\Rust\the_rust_programming_language\webserver_hello\target\debug\deps\webserver_hello.d: src\main.rs
+
+src\main.rs:
---
+++ webserver_hello/target/debug/webserver_hello.d
@@ -0,0 +1,1 @@
+D:\Prog\Rust\the_rust_programming_language\webserver_hello\target\debug\webserver_hello.exe: D:\Prog\Rust\the_rust_programming_language\webserver_hello\src\main.rs
